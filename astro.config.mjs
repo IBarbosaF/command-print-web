c71d3881def3b5d15a6e46be763dc52eb830e8f3
IBarbosaF <ibarbosaferrer91@gmail.com>
2026-07-30 20:16:41 +0200

    Configuracion despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://IBarbosaF.github.io',
+  base: '/command-print-web',
   vite: {
     plugins: [tailwindcss()]
   }
